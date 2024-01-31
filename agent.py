26002fce1c9bb5494c5e7e2d8e5f065666889771
Frederico Wieser <20886677+fredericowieser@users.noreply.github.com>
2024-01-31 23:23:19 +0000

    Initial Commit

---
+++ agent.py
@@ -0,0 +1,19 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from snake_AI import SnakeGameAI
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def get_state(self, game):
+        pass
+
+    def remember
+
